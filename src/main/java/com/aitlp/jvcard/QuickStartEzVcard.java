b92328633e5f92280fdaae8cbde311f1189205e6
aitlp <aitlpmw@gmail.com>
2019-10-07 21:18:20 +0800

    增加读取、写入使用示例；

--- src/main/java/com/aitlp/jvcard/QuickStartEzVcard.java
+++ src/main/java/com/aitlp/jvcard/QuickStartEzVcard.java
@@ -3,18 +3,29 @@ package com.aitlp.jvcard;
 import ezvcard.Ezvcard;
 import ezvcard.VCard;
 import ezvcard.parameter.EmailType;
+import org.apache.commons.lang3.ObjectUtils;
+import org.apache.commons.lang3.StringUtils;
 
-import java.io.File;
-import java.io.IOException;
+import java.io.*;
 import java.util.ArrayList;
 import java.util.List;
 
 public class QuickStartEzVcard {
 
     public static void main(String[] args) {
-        VCard vCard = new VCard();
-        vCard.addEmail("4spaces.com@gmail.com", EmailType.INTERNET);
-        List<VCard> vcards = new QuickStartEzVcard().readVcardFile("C:\\Users\\michael\\Downloads\\contacts.vcf");
+        List<String> whiteListEmails = new QuickStartEzVcard().readEmails("F:\\jvcard\\src\\main\\java\\com\\aitlp\\jvcard\\whitelist.txt");
+        List<String> emails = new QuickStartEzVcard().readEmails("F:\\飞飞影视群.txt");
+        List<VCard> vcards = new ArrayList<>();
+        if (ObjectUtils.isNotEmpty(emails)) {
+            for (String email : emails) {
+                if(!whiteListEmails.contains(email)){
+                    VCard vCard = new VCard();
+                    vCard.addEmail(email, EmailType.INTERNET);
+                    vcards.add(vCard);
+                }
+            }
+            new QuickStartEzVcard().writeVcardFile(vcards,"F:\\ALLEMAILS.vcf");
+        }
     }
 
     /**
@@ -52,4 +63,34 @@ public class QuickStartEzVcard {
         return true;
     }
 
+    /**
+     * 读取邮件(一行一个)
+     *
+     * @return
+     */
+    public List<String> readEmails(String fileFullPath) {
+        List<String> emails = new ArrayList<>();
+        FileInputStream fileInputStream;
+        try {
+            InputStreamReader inputStreamReader;
+            BufferedReader bufferedReader;
+
+            File filePath = new File(fileFullPath);
+            // 将文件读入输入流
+            fileInputStream = new FileInputStream(filePath);
+            inputStreamReader = new InputStreamReader(fileInputStream);
+            bufferedReader = new BufferedReader(inputStreamReader);
+            // 读取原有的内容
+            String line;
+            while (bufferedReader.ready()) {
+                line = bufferedReader.readLine();
+                if (StringUtils.isNotBlank(StringUtils.trimToEmpty(line))) {
+                    emails.add(line);
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return emails;
+    }
 }
